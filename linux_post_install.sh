e2f6146c86d838393b41d12430c5c25f89a525c3
Mike Brewer <mbrew345@gmail.com>
2020-07-17 09:13:28 -0400

    Init Script Upload

---
+++ linux_post_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+YUM_PACKAGE_NAME="epel-release xe-guest-utilities-latest htop sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python nano"
+DEB_PACKAGE_NAME="python2.7 python-dev python-pip libssl-dev"
+
+ if cat /etc/*release | grep ^NAME | grep CentOS; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on CentOS"
+    echo "==============================================="
+    yum install -y $YUM_PACKAGE_NAME
+    systemctl enable xe-linux-distribution
+    systemctl start xe-linux-distribution
+
+
+ elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
+    echo "==============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Debian ; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Debian"
+    echo "==============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ else
+    echo "OS NOT DETECTED, couldn't install package $PACKAGE"
+    exit 1;
+ fi
+
+exit 0
